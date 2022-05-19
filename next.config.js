f8ad955106c210e69c84fef8f1b459177d0d9a32
David Caro <david.caro.dec@hotmail.com>
2022-05-19 10:58:46 -0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
-// const path = require('path');
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
   rectStrictMode: true,
-  // exportPathMap: function () {
-  //   return {
-  //     "/": { page: "/" },
-  //     "/burguer": { page: "/[food]" },
-  //     "/pizza": { page: "/[food]" },
-  //     "/coffee": { page: "/[food]" },
-  //     "/oriental": { page: "/[food]" },
-  //     "/deserts": { page: "/[food]" }
-  //   }
-  // },
   assetPrefix: !debug ? '/nextjs-restaurantapp/' : '',
-  basePath: "/nextjs-restaurantapp"
+  basePath: !debug ? "/nextjs-restaurantapp" : '' 
 }
