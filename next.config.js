e2853864ba7301cb4ad9317759b1b476d0e4f96f
David Caro <david.caro.dec@hotmail.com>
2022-05-25 15:38:33 -0300

    predeploy

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const debug = process.env.NODE_ENV !== "production";
-
-module.exports = {
-  rectStrictMode: true,
-  assetPrefix: !debug ? '/nextjs-restaurantapp/' : '',
-  basePath: !debug ? "/nextjs-restaurantapp" : '' 
-}
